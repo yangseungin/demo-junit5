d154c1614e538929d5f2202dfb36a15e6ea77598
seungin yang <rhfpdk92@naver.com>
2022-03-21 23:10:30 +0900

    요구사항 정리

---
+++ app/src/test/java/tdd/ExpireDateCalculatorTest.java
@@ -0,0 +1,11 @@
+package tdd;
+
+public class ExpireDateCalculatorTest {
+
+//    TODO 서비스를 사용하려면 매달 1만원을 선불로 납부한다. 납부일 기준으로 한달뒤가 서비스 만료일이 된다.
+//    TODO 2개월 이상 요금을 납부할 수 있따.
+//    TODO 10만원을 납부하면 서비스를 1년 제공한다.
+
+
+
+}
